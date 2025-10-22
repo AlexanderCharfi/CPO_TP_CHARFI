2d347503a52d7f9ba2ec67f292bf68ee9e88f49f
Alexander <Alexander@10.3.62.151>
2025-10-22 15:31:56 +0200

    TP0_Exercice_addition

---
+++ TP0_addition/src/tp0_addition/TP0_addition.java
@@ -0,0 +1,37 @@
+// CHARFI Alexander 20/10/2025 TP0_addition
+package tp0_addition;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexander
+ */
+public class TP0_addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nb; // nombre d'entiers à additionner
+        int result; // resultat
+        int ind; //indice
+        
+        // initialisation
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le nombre :");
+        nb=sc.nextInt ();
+        result=0;
+        
+        // addition des nb premiers entiers
+        ind=1;
+        
+        while (ind< nb) {
+            result=result+ind;
+            ind++;
+        }
+        // affichage du resultat
+        System.out.println("la somme des "+nb + " entiers est: "+result);
+    }
+    
+}
