83197b40308022caa2b6efb4ce3ea84a36f57b1c
Alexander <Alexander@10.3.62.151>
2025-12-12 17:20:56 +0100

    TP_miniProjet_Interface    12/12/2025  17:20
    -V1 terminer
    -Ecran Titre fait
    -Ecran Défaite fait
    -Ecran Victoire fait
    -Modes de difficultés fait
    -Tous les boutons Quitter fait

--- miniprojet_cadenas_CHARFI/src/ecran_defaite.java
+++ miniprojet_cadenas_CHARFI/src/ecran_defaite.java
@@ -17,7 +17,7 @@ public class ecran_defaite extends javax.swing.JFrame {
     public ecran_defaite() {
         initComponents();
     }
-
+//
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
